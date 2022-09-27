3d3a2936bd49cc4e607fe89aeb943bf8f789e3b3
Daniel Ayanaba <ayanaba60@gmail.com>
2022-09-27 17:47:58 +0100

    Update main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,7 +1,5 @@
 #ifndef MAIN_H 
-#define MAIN_H                                                                                                              
-
-                                                                                                                            
+#define MAIN_H                                                                                                                                                                                                                                        
 
 /**                                                                                                                         
  *
