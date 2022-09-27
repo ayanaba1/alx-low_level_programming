a5b351301252043fca7d74dc411c31751587e7c7
Daniel Ayanaba <ayanaba60@gmail.com>
2022-09-27 17:45:13 +0100

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,46 +1,21 @@
 /**                                                                                                                         
 
  * _memset - fills a mermory block with a constant byte                                                                     
-
  * @s: address to memory block                                                                                              
-
  * @b: char to be used                                                                                                      
-
- * @n: number of bytes to be used                                                                                           
-
+ * @n: number of bytes to be used                                                                                          
  *                                                                                                                          
-
  * Return: pointer to the memory block                                                                                      
-
- */                                                                                                                         
+ */ 
 
 char *_memset(char *s, char b, unsigned int n)                                                                              
 
 {                                                                                                                           
-
         while (n)                                                                                                           
-
         {                                                                                                                   
 
-                s[n - 1] = b;                                                                                               
-
+               s[n - 1] = b;                                                                                               
                 n--;                                                                                                        
-
         }                                                                                                                   
-
         return (s);                                                                                                         
-
-}                                                                                                                           
-
-~                                                                                                                           
-
-~                                                                                                                           
-
-~                                                                                                                           
-
-~                                                                                                                           
-
-~                                                                                                                           
-
-~                  
-
+}                                                                                                                                     
