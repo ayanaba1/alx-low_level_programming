11558ab064961037afd0508be4d7a216e4b2622c
Daniel Ayanaba <ayanaba60@gmail.com>
2022-09-29 00:13:27 +0100

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,18 +1,17 @@
-include "main.h"                                                                                                                                                                                                         
+include "main.h"                                                                                             
+                                                                                                             
 /**                                                                                                          
- * _puts_recursion - prints's a string followed by a new line                                                
- * @s: string to be printed                                                                                  
- *                                                                                                           
- * Return: void                                                                                              
+ * _puts_recursion - function                                               
+ * @s: input                                                                                                                                                                                         
+ * Return: Always 0                                                                                              
  */                                                                                                          
 void _puts_recursion(char *s)                                                                                
 {                                                                                                            
-        if (*s == '\0')                                                                                      
-        {                                                                                                    
-               _putchar('\n');                                                                               
-        }                                                                                                    
+        if (*s == '\0')                                                                                                                                                                                  
+               _putchar('\n');                                                                                                                                                                                
         else                                                                                                 
         {                                                                                                    
-               _putchar(s[0]);                                                                               
+               _putchar(*s);                                                                               
                _puts_recursion(s + 1);                                                                       
         }                                                                                                    
+                               
