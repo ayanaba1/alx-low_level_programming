4b18448bfa786609b95e33d92152b3e6e1e8af12
Daniel Ayanaba <ayanaba60@gmail.com>
2022-09-28 23:39:40 +0100

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,19 +1,19 @@
-#ifndef MAIN_H                                                                                             
-#define MAIN_H                                                                                             
-                                                                                                           
-#include <stdio.h>                                                                                         
-                                                                                                           
-int _putchar(char c);                                                                                      
-void _puts_recursion(char *s);                                                                             
-void _print_rev_recursion(char *s);                                                                        
-int _strlen_recursion(char *s);                                                                            
-int factorial(int n);                                                                                      
-int _pow_recursion(int x, int y);                                                                          
-int _sqrt_recursion(int n);                                                                                
-int is_prime_number(int n);                                                                                
-int _sqrt(int n, int x);                                                                                   
-int is_prime(int n, int start);                                                                            
-void check(char *s, int start, int end, int *flag);                                                        
-int wildcmp(char *s1, char *s2);                                                                           
-                                                                                                           
-#endif /*MAIN_H*/    
+include "main.h"                                                                                             
+                                                                                                             
+/**                                                                                                          
+ * _puts_recursion - prints's a string followed by a new line                                                
+ * @s: string to be printed                                                                                  
+ *                                                                                                           
+ * Return: void                                                                                              
+ */                                                                                                          
+void _puts_recursion(char *s)                                                                                
+{                                                                                                            
+        if (*s == '\0')                                                                                      
+        {                                                                                                    
+               _putchar('\n');                                                                               
+        }                                                                                                    
+        else                                                                                                 
+        {                                                                                                    
+               _putchar(s[0]);                                                                               
+               _puts_recursion(s + 1);                                                                       
+        }                                                                                                    
