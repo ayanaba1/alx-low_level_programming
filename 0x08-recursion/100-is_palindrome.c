27eb8fa5ffffaef1426d8c2a757059e45ec98c05
Daniel Ayanaba <ayanaba60@gmail.com>
2022-09-29 18:52:33 +0100

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,32 +1,29 @@
 #include "main.h"
 
 /**
- * is_palindrome - return the 1 if s is a palindrom
- * @s: string to be check
+ * is_palindrome - returns the 1 if s is a palindrome
+ * @s: string to be checked
  *
  * Return: 1 if s is a palindrome, 0 otherwise
  */
-
 int is_palindrome(char *s)
-
 {
 	int flag = 1;
-	check(s, o, _strlen_recursion(s) - 1, &flag);
+
+	check(s, 0, _strlen_recursion(s) - 1, &flag);
 	return (flag);
 }
 
 /**
- * check - check if a string is palindrome
+ * check - checks if a string is a palindrome
  * @s: string to be checked
  * @start: start index
  * @end: end index
- * @flag: flag to indicate of a string is a palindrome
+ * @flag: flag to indicate if a string is a palindrome
  *
  * Return: void
  */
-
 void check(char *s, int start, int end, int *flag)
-
 {
 	if (start <= end)
 	{
@@ -39,21 +36,19 @@ void check(char *s, int start, int end, int *flag)
 }
 
 /**
- * _strlen_recursion - calculate the length of a string
+ * _strlen_recursion - calculates the length of a string
  * @s: string to be used
  *
  * Return: length of the string
  */
-
 int _strlen_recursion(char *s)
-
 {
 	int sum = 0;
 
 	if (*s != '\0')
 	{
 		sum++;
-		sum + = _strlen_recursion(s + 1);
+		sum += _strlen_recursion(s + 1);
 	}
 	return (sum);
 }
