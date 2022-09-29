d5736bf5649a6748a5009dc657b1ab306b7fd66d
root <ayanaba1>
2022-09-28 18:04:54 -0700

    updates

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "main.h"
 
 /**
@@ -16,24 +15,3 @@ void _puts_recursion(char *s)
 		_puts_recursion(s + 1);
 	}
 }
-
-
-=======
-#include "main.h"                                                                                            
-                                                                                                             
-/**                                                                                                          
- * _puts_recursion - function                                                                                
- * @s: input                                                                                                 
- * Return: Always 0                                                                                          
- */                                                                                                          
-void _puts_recursion(char *s)                                                                                
-{                                                                                                            
-        if (*s == '\0')                                                                                      
-                _putchar('\n');                                                                              
-        else                                                                                                 
-        {                                                                                                    
-                _putchar(*s);                                                                                
-                _puts_recursion(s + 1);                                                                      
-        }                                                                                                    
-}                                                                                                           
->>>>>>> a103e1bea001509da03eb0936cc00dd417e81ebe
