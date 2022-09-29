a338bed654b5c117b366bdeb790b779925956f03
root <ayanaba1>
2022-09-28 17:33:30 -0700

    modified files

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
-#define MAIN_H                                                                                                           i
+#define MAIN_H
+
 void _puts_recursion(char *s);
 int _putchar(char c);
 void _print_rev_recursion(char *s);
