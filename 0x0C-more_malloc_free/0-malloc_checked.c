fcb6294bb02bccc366074c6d9693ba978620e931
Daniel Ayanaba <ayanaba60@gmail.com>
2022-11-15 12:09:00 +0100

    Update 0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,21 +1,19 @@
-#include "main.h"
-#include <stdlib.h>
-
-/**
- * malloc_checked - allocates memory using malloc
- * @b: memory to allocate
- *
- * Return: pointer to allocated memory or normal process termination if error
- * /
-void *malloc_checked(unsigned int b)
-{
+#include "main.h"                                                                                                                       
+#include <stdlib.h>                                                                                                                     
+                                                                                                                                        
+/**                                                                                                                                     
+ * malloc_checked - allocates memory using malloc                                                                                       
+ * @b: memory to allocate                                                                                                               
+ *                                                                                                                                      
+ * Return: pointer to allocated memory or normal process termination if error                                                           
+ */                                                                                                                                    
+void *malloc_checked(unsigned int b)                                                                                                    
+{                                                                                                                                                                                                                                                                         
+      void *p;
  
-      void *p:
-
-      p = malloc(b):
-      if (p == NULL)
-             exit(98);
-      return (p);
-}  
-
-
+      p = malloc(b);                                                                                                                   
+      if (p == NULL)                                                                                                                    
+             exit(98);                                                                                                                  
+      return (p);                                                                                                                       
+}                                                                                                                                    
+                        
