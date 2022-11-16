27b35473c65557d0f9d68470bbb1a8ba11fe8ab8
Daniel Ayanaba <ayanaba60@gmail.com>
2022-11-16 17:39:59 +0100

    Update 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,37 +1,19 @@
-nclude <stdio.h>
-
+#include <stdio.h>
 /**
- *
- * * main - Prints number of arguments received
- *
- * * @argc: Number of arguments
- *
- * * @argv: arguments
- *
- * * Return: 0
- *
- * */
-
-
+* main - Prints number of arguments received
+* @argc: Number of arguments
+* @argv: arguments
+* Return: 0
+*/
 
 int main(int argc, char **argv)
-
 {
+	int i;
 
-		int i;
-
-
-
-			if (argc >= 1)
-
-					{
-
-							for (i = 0; i < argc ; i++)
-
-										printf("%s\n", argv[i]);
-
-								}
-
-				return (0);
-
+	if (argc >= 1)
+	{
+	for (i = 0; i < argc ; i++)
+		printf("%s\n", argv[i]);
+	}
+	return (0);
 }
