ad38b0764079a165a705e08cf0954d6e5d39ec2f
root <ayanaba1>
2022-11-16 07:53:09 -0800

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,27 @@
+nclude <stdio.h>
+
+/**
+ *
+ * * main - Prints number of arguments
+ *
+ * * @argc: Number of arguments
+ *
+ * * @argv: arguments
+ *
+ * * Return: 0
+ *
+ * */
+
+
+
+int main(int argc, char **argv)
+
+{
+
+		if (*argv)
+
+				printf("%d\n", argc - 1);
+
+			return (0);
+
+}
